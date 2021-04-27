13a3578a4ace77ebbc4c2bd0d0365a74646088ba
Willyan <willyanf17@hotmail.com>
2021-04-27 15:43:19 -0300

    initial commit

---
+++ app/src/main/java/me/willyan/ichat/LoginActivity.java
@@ -0,0 +1,50 @@
+package me.willyan.ichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private EditText editUser, editPassword;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editUser = findViewById(R.id.editUser);
+        editPassword = findViewById(R.id.editPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = editUser.getText().toString();
+                String password = editPassword.getText().toString();
+
+                Log.i("Teste", login);
+                Log.i("Teste", password);
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
