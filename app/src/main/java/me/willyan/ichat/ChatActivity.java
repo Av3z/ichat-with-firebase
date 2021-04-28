c1468df1d11cbedfb6df8386e08a72642003b767
Willyan <willyanf17@hotmail.com>
2021-04-28 20:55:02 -0300

    create chat activity

---
+++ app/src/main/java/me/willyan/ichat/ChatActivity.java
@@ -0,0 +1,58 @@
+package me.willyan.ichat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private GroupAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerChat);
+        adapter = new GroupAdapter();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(true));
+
+    }
+
+
+    private class MessageItem extends Item<ViewHolder> {
+
+        private final boolean isRight;
+
+        private MessageItem(boolean isRight) {
+            this.isRight = isRight;
+        }
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+
+        }
+
+        @Override
+        public int getLayout() {
+            return isRight ? R.layout.item_to_message : R.layout.item_from_message;
+        }
+    }
+}
--- app/src/main/java/me/willyan/ichat/ContactsActivity.java
+++ app/src/main/java/me/willyan/ichat/ContactsActivity.java
@@ -1,29 +1,55 @@
 package me.willyan.ichat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.OnItemClickListener;
+import com.xwray.groupie.ViewHolder;
 
-import java.util.Collection;
 import java.util.List;
 
 import me.willyan.ichat.model.User;
 
 public class ContactsActivity extends AppCompatActivity {
 
+    private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+
+        adapter = new GroupAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(@NonNull Item item, @NonNull View view) {
+                Intent i = new Intent(ContactsActivity.this, ChatActivity.class);
+                startActivity(i);
+            }
+        });
 
         fetchContacts();
     }
@@ -41,10 +67,37 @@ public class ContactsActivity extends AppCompatActivity {
                         List<DocumentSnapshot> docs = value.getDocuments();
                         for(DocumentSnapshot doc : docs){
                             User user = doc.toObject(User.class);
+                            adapter.add(new UserItem(user));
                             Log.d("Teste", user.getName());
 
                         }
                     }
                 });
+    }
+
+
+    private class UserItem extends Item<ViewHolder>{
+
+        private final User user;
+
+        private UserItem(User user) {
+            this.user = user;
+        }
+
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            TextView textViewName =  viewHolder.itemView.findViewById(R.id.textContact);
+            ImageView imageContacts = viewHolder.itemView.findViewById(R.id.imgPhotoContacts);
+
+            textViewName.setText(user.getName());
+            Picasso.get().load(user.getPhotoUrl()).into(imageContacts);
+
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_user;
+        }
     }
 }
