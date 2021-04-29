794da7f03cfcd128693555b9cf8a8afb74b5b984
Willyan <willyanf17@hotmail.com>
2021-04-29 00:15:11 -0300

    create model contact

---
+++ app/src/main/java/me/willyan/ichat/model/Contact.java
@@ -0,0 +1,50 @@
+package me.willyan.ichat.model;
+
+public class Contact {
+
+    private String uuid;
+    private String name;
+    private String lastMessage;
+    private String photoUrl;
+    private long timestamp;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
